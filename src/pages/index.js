af4f750c4c7086d77fd4c0ab51406b8e3e8e1006
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-10 12:54:09 +0530

    Added React. V1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const getStaticProps = async () => {
-  const res = await fetch('YOUR_API_URL')
+  const res = await fetch('https://api.lanyard.rest/v1/users/890232380265222215')
   const data = await res.json()
 
   return {
