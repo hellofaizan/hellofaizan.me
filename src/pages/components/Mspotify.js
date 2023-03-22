88d74fb26afbf7eab63f880f12d35016df2814e4
Mohammad Faizan <faizancurious@gmail.com>
2023-03-22 20:16:30 +0530

    More Changes

--- src/pages/components/Mspotify.js
+++ src/pages/components/Mspotify.js
@@ -19,26 +19,28 @@ const Spotify = () => {
     }, [data]) //data.data?.listening_to_spotify
     if (data.data?.listening_to_spotify === true) {
         return (
-            <div className='flex sm:hidden justify-between gap-2'>
-            <div className='flex flex-col justify-start items-start'>
-                <h1 className='text-[#1e1e1e] dark:text-[#fff] font-bold text-sm text-ellipsis'>Listening {data?.data?.spotify?.song}</h1>
-                <h1 className='text-[#3e3e3e] dark:text-[#c1c1c1] text-sm'>{data?.data?.spotify?.artist} - Spotify</h1>
-            </div>
+            <div className='flex sm:hidden justify-between gap-2 mb-5'>
+                <div className='flex flex-col justify-start items-start'>
+                    <h1 className='text-[#1e1e1e] dark:text-[#fff] font-bold text-sm text-ellipsis'>Listening <a href={`https://open.spotify.com/track/${data?.data?.spotify?.track_id}`} target="_blank" rel="noreferrer" className="w-full font-medium text-gray-900 dark:text-[#e1eafd] hover:text-yellow-400 truncate" >
+                        {data?.data?.spotify?.song} <i className='bi bi-box-arrow-up-right'></i>
+                    </a></h1>
+                    <h1 className='text-[#3e3e3e] dark:text-[#c1c1c1] text-sm'>{data?.data?.spotify?.artist} - Spotify</h1>
+                </div>
 
-            <i className='bi bi-spotify text-3xl flex justify-center items-center text-[#3e3e3e] dark:text-[#c1c1c1]'></i>
-            
-        </div>
+                <i className='bi bi-spotify text-3xl flex justify-center items-center hover:text-green-500 text-[#3e3e3e] dark:text-[#c1c1c1]'></i>
+
+            </div>
         )
     }
     return (
-        <div className='flex sm:hidden justify-between gap-2'>
+        <div className='flex justify-between gap-2 mb-5'>
             <div className='flex flex-col justify-start items-start'>
                 <h1 className='text-[#1e1e1e] dark:text-[#fff] font-bold text-base'>Not listening to</h1>
                 <h1 className='text-[#3e3e3e] dark:text-[#c1c1c1] text-sm'>Spotify</h1>
             </div>
 
-            <i className='bi bi-spotify text-3xl flex justify-center items-center text-[#3e3e3e] dark:text-[#c1c1c1]'></i>
-            
+            <i className='bi bi-spotify text-3xl flex justify-center items-center hover:text-green-500 text-[#3e3e3e] dark:text-[#c1c1c1]'></i>
+
         </div>
     )
 }
