e9d684d8329c43797fb10f9dd6ac7854ff42b0b3
Mohammad Faizan <faizancurious@gmail.com>
2023-03-28 10:35:34 +0530

    V1.3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Home = ({mosam}) => {
-  console.log(mosam)
   const [stats, setStats] = useState({})
   const [topRepos, setTopRepos] = useState([])
   useEffect(() => {
